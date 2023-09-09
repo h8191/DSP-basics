f9cc3e4da15623d7e7bf409c85a9fa6f0f27bdef
h8191 <45847870+h8191@users.noreply.github.com>
2023-09-09 16:46:07 +0530

    Update my_fft.py move frequently reused logic to __init__

--- my_fft.py
+++ my_fft.py
@@ -12,10 +12,7 @@ import matplotlib.pyplot as plt
 
 data = np.random.randint(-1,2,32768)*64
 
-def my_fft(data):
-    return data
-
-for fft_func in [np.fft.fft, my_fft]:
+for fft_func in [np.fft.fft,]:
     for fft_size in [1024,8192,32768]:
         t1 = time.time()
         f_abs = np.abs(fft_func(data[:fft_size]))
@@ -23,8 +20,9 @@ for fft_func in [np.fft.fft, my_fft]:
 
 
 class fft_calc(object):
-    def __init__(self,N=1024):
+    def __init__(self,N=1024,ROUND=None):
         self.N = N
+        self.ROUND  = ROUND
         self.k_arr  = np.arange(self.N)
         self.Wn_arr = np.exp(-2j*np.pi/self.N * np.arange(self.N+1))
 
@@ -33,18 +31,15 @@ class fft_calc(object):
         self.lookup_8bit_rev = [eval('0b'+bin(i)[2:].zfill(         8)[::-1]) for i in range(1<<8)     ]
         self.lookup_Nbit_rev = [eval('0b'+bin(i)[2:].zfill(self.log2N)[::-1]) for i in range(1<<self.log2N)] #try a more efficient approach
 
-    def fft(self,Input,ROUND=None):
-        stage_Input = Input[self.lookup_Nbit_rev]
+        mask_bf1 = np.zeros((self.log2N, self.N), dtype=bool) #mask for part1 of butterfly
+        mask_bf2 = np.zeros((self.log2N, self.N), dtype=bool) #mask for part2 of butterfly
+
+        self.stage_Weights = []
 
         for i in range(self.log2N):
-            mask_bf1 = np.zeros(self.N, dtype=bool) #mask for part1 of butterfly
-            mask_bf2 = np.zeros(self.N, dtype=bool) #mask for part2 of butterfly
-
-            stage_Output = np.zeros_like(stage_Input, dtype=np.complex128)
-
-            mask_bf2[((self.k_arr >> i)&1)!=0] = 1 #shouldn't be an issues even if we consider numpy's rotation shifting instead of normal shifting
+            mask_bf2[i,((self.k_arr >> i)&1)!=0] = 1 #shouldn't be an issues even if we consider numpy's rotation shifting instead of normal shifting
+            mask_bf1[i] = ~mask_bf2[i]
             #print(mask_bf2.sum(),'sum', i)
-            mask_bf1 = ~mask_bf2
 
             rep_cnt = 1<<(self.log2N-i-1)
             weight_lookup_ind = np.tile(np.arange(0,self.N//2,rep_cnt),reps=[rep_cnt])
@@ -58,11 +53,23 @@ class fft_calc(object):
                 else:
                     assert False
 
-            G  = stage_Input[ mask_bf1]
-            H1 = stage_Input[ mask_bf2] * stage_Weight
+            self.stage_Weights.append(stage_Weight)
 
-            stage_Output[ mask_bf1] = G + H1
-            stage_Output[ mask_bf2] = G - H1
+        self.mask_bf1 = mask_bf1
+        self.mask_bf2 = mask_bf2
+
+    def fft(self,Input):
+        stage_Input = Input[self.lookup_Nbit_rev]
+
+        for i in range(self.log2N):
+
+            stage_Output = np.zeros_like(stage_Input, dtype=np.complex128)
+
+            G  = stage_Input[ self.mask_bf1[i]]
+            H1 = stage_Input[ self.mask_bf2[i]] * self.stage_Weights[i]
+
+            stage_Output[     self.mask_bf1[i]] = G + H1
+            stage_Output[     self.mask_bf2[i]] = G - H1
 
             stage_Input = stage_Output
 
@@ -80,21 +87,21 @@ def my_bin_round(x,ROUND):
 
 
 for i00 in range(17,9,-1):
+    #fft_calc_obj = fft_calc(N=(1<<i00), ROUND=None)
+    #fft_calc_obj = fft_calc(N=(1<<i00), ROUND=1) 
+    fft_calc_obj = fft_calc(N=(1<<i00), ROUND='0b11') 
     t1 = time.time()
-    fft_calc_obj = fft_calc(N=(1<<i00))
     data = np.sin(2*np.pi*0.1*np.arange(fft_calc_obj.N)) * 120
-    fft1 = fft_calc_obj.fft(data[:fft_calc_obj.N], ROUND=None) 
-    #fft1 = fft_calc_obj.fft(data[:fft_calc_obj.N], ROUND=1) 
-    #fft1 = fft_calc_obj.fft(data[:fft_calc_obj.N], ROUND='0b11') 
+    fft1 = fft_calc_obj.fft(data[:fft_calc_obj.N])
     fft2 =       np.fft.fft(data[:fft_calc_obj.N])
     fft1 /= len(fft1)
     fft2 /= len(fft2)
     fft_diff = fft1 - fft2
     fft_diff_abs = np.abs(fft_diff)
-    print(np.allclose(fft1,fft2),fft_diff_abs.mean(),fft_diff_abs.max(), time.time()-t1, flush=True)
-plt.figure()
-plt.subplot(1,2,1);plt.plot(np.abs(fft1))
-plt.subplot(1,2,2);plt.plot(np.abs(fft2)) 
-plt.show()
+    print(np.allclose(fft1,fft2),fft_diff_abs.mean(),fft_diff_abs.max(), time.time()-t1, i00, flush=True)
+#plt.figure()
+#plt.subplot(1,2,1);plt.plot(np.abs(fft1))
+#plt.subplot(1,2,2);plt.plot(np.abs(fft2)) 
+#plt.show()
 
 
